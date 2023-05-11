c27e086d7af41fd160a25e8bed65f3c72372421d
rajitha-00 <rajithatw07102@gmail.com>
2023-05-11 21:31:30 +0100

    shop 30% done

--- src/pages/HomePagePet.vue
+++ src/pages/HomePagePet.vue
@@ -2,13 +2,13 @@
     <div class="wrapper" id="home">
         <div class="inner-wrapper">
 
-            <section class="items-start justify-center ">
-                <img class="heroImage" src="../assets/images/home/heroTheme.png" alt="">
+            <section class="md:items-start md:justify-center ">
+                <img class="heroImage " src="../assets/images/home/heroTheme.png" alt="">
                 <div class="tipsContainer">
                     <div class="tips">
                         <div class="tipBox justify-center text-center">
                             <div class="tipText text-center">
-                                <h1 class=" text-xl" >Tips,</h1>
+                                <h1 class=" md:text-xl" >Tips,</h1>
                                 <!-- tip1 -->
                                 <p> Regular checkups are essential for ensuring your dog's health and well-being. 
                                     Your vet can provide preventive care, such as vaccinations, parasite prevention, and dental care, as well as diagnose and treat any health issues that arise.</p>
--- src/pages/ShopPet.vue
+++ src/pages/ShopPet.vue
@@ -1,8 +1,29 @@
 <template>
+   
+    <div class="vectorShop">
+        <img src="../assets/images/shop/Vector.png" alt="">
+    </div>
     <div class="wrapper" id="shop">
         <div class="inner-wrapper">
             <section >
-                shop
+                <div class="categories">
+                    <div class="category">
+                        <div class="category-title">
+                            <h2>Category</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-box">
+                    <div class="item">
+                        <div class="item-title">
+                            <h2>Item</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="watermark">
+                    <img src="../assets/images/shop/water.png" alt="">
+                </div>
             </section>
         </div>
     </div>
