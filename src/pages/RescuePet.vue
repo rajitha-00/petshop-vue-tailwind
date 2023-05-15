3e650cae8e5709e808f1163d0017ff14f9e33d1f
rajitha-00 <rajithatw07102@gmail.com>
2023-05-15 13:49:23 +0100

    rescue done

--- src/pages/RescuePet.vue
+++ src/pages/RescuePet.vue
@@ -1,22 +1,38 @@
 <template>
     <div class="wrapper rescue" id="rescue">
-        <div class="inner-wrapper">
+        <div class="inner-wrapper mb-20 md:mb-0">
 
-            <section class="text-start px-4 mb-10">
-                <div class="section-cat-header text-brown-600 px-12  md:flex items-start justify-between">
-                    <div class="section-left md:w-1/2 flex w-full md:justify-end items-center">
-                        
-                        <div class="rescueBox flex items-center">
+            <section class="text-start  ">
+                <div class="section-rescue-header text-brown-600   md:flex items-center justify-between">
+                    <div class="section-left-rescue h-full md:w-2/3 flex w-full justify-center md:justify-end items-center">
+                        <div class="rescueBox flex items-center shadow justify-center">
                             <img src="../assets/images/rescue/rescuepet.png" class="w-96" alt="">
+                        </div>
+                        <div>
+
                         </div>
                        
                     </div>
-                    <div class="section-right md:w-1/2 w-full">
-                        <div class="title">
+                    <div class="section-right-rescue flex md:w-2/3 w-full items-center justify-center ">
+                        <div class="title w-96">
                             <h2>Sherlock Rescue</h2>
                             <hr class="w-full">
+                            <br/>
+                            <p>
+                                Sherlock Rescue is a dedicated team of animal lovers who are committed to rescuing and rehabilitating dogs in need. Our mission is to provide a safe and loving environment for dogs who have been abandoned, abused, or neglected, and to find them permanent homes where they can thrive and live happy lives.
+
+                                <br/>
+                                <br/>
+                                At Sherlock Rescue, we believe that every dog deserves a second chance, and we work tirelessly to provide the care and support that these animals need to heal and recover. Our team of skilled and compassionate volunteers includes experienced animal handlers, trainers, and veterinary professionals who are dedicated to providing the best possible care for our dogs.
+                                <br/>
+                                <br/>
+                                Our rescue center is a place of hope and healing, where dogs receive the medical care, nutrition, and socialization they need to prepare for adoption. We work closely with local animal shelters, veterinary clinics, and other rescue organizations to ensure that every dog in our care has the best chance for a bright future.
+                            </p>
                         </div>
                     </div>
+                </div>
+                <div class=" absolute flex justify-end w-full items-end h-20 px-12 md:bottom-10 md:right-5">
+                    <img src="../assets/images/rescue/rescuedown.png" alt="">
                 </div>
             </section>
         </div>
