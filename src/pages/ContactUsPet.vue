bc0ba769934ee81ab7680d9530c09a1b156e43b7
rajitha-00 <rajithatw07102@gmail.com>
2023-05-16 07:36:58 +0100

    done

--- src/pages/ContactUsPet.vue
+++ src/pages/ContactUsPet.vue
@@ -12,44 +12,64 @@
               </div>
             </div>
           </div>
-          <div class="flex">
+          <div class="flex items-center justify-center">
 
-              <div class="contactCard text-white w-4/5 rounded p-10 " >
+              <div class="contactCard text-white w-4/5 rounded px-10 py-20 " >
                   <div class="cardHeader-contact ">
-                  <div class="title text-center">
+                  <div class="title text-center mb-10" >
                       <p>We love hearing from our customers and fellow pet lovers! Here's how you can get in touch with us:</p>
                   </div>
                   </div>
-                  <div class="flex  items-center justify-around">
+                  <div class="flex flex-wrap  items-start justify-around">
   
                       <div class="cardLeft text-sm ">
-                      <p class="my-5">
+                      <p class="mb-6">
                           Address: <br>
                           Jack Pet Supplies and Sherlock Rescue, <br>
                           542 Peradeniya Rd, <br>
                           Kandy, Sri Lanka 20000
                       </p>
-                      <p class="my-5">
+                      <p class="mb-6">
                           Phone: (+94)817 818 181
                       </p>
-                      <p class="my-5">
+                      <p class="mb-6">
                           Email: info@jackpetsupplies.com
                       </p>
-                      <p class="my-5">
+                      <p class="mb-6">
                           WhatsApp at (+94)817 818 181
                       </p>
                       </div>
                       <div class="cardRight">
-                      <div class="signUpCard">
-                          <p>SIGN-UP TO GET YOUR COUPON CODE </p>
-                      </div>
-                      <div class="socialCard">
-                          <div class="socialIcon " v-for="(ico, index) in Icons" :key="index">
-                          <a :href="ico.link">
-                              <img :src="ico.path" alt="">
-                          </a>
-                          </div>
-                      </div>
+                        <div class="signUpCard bg-white py-12 px-10 rounded text-stone-900">
+                            <p>SIGN-UP TO GET YOUR COUPON CODE </p>
+                        </div>
+                        <div class="socialCard flex justify-between mt-10">
+                            <div class="socialIcon w-14 h-14" >
+                                <a href="#">
+                                    <img src="../assets/images/contactus/Instagram.png" alt="">
+                                </a>
+                            </div>
+                            <div class="socialIcon w-14 h-14 " >
+                                <a href="#">
+                                    <img src="../assets/images/contactus/YouTube.png" alt="">
+                                </a>
+                            </div>
+                            <div class="socialIcon w-14 h-14" >
+                                <a href="#">
+                                    <img src="../assets/images/contactus/Facebook.png" alt="">
+                                </a>
+                            </div>
+                            <div class="socialIcon w-14 h-14" >
+                                <a href="#">
+                                    <img src="../assets/images/contactus/Telegram.png" alt="">
+                                </a>
+                            </div>
+                            <div class="socialIcon w-14 h-14" >
+                                <a href="#">
+                                    <img src="../assets/images/contactus/TikTok.png" alt="">
+                                </a>
+                            </div>
+                        </div>
                       </div>
                   </div>
               </div>
@@ -62,16 +82,6 @@
   <script>
   export default {
     name: "ContactUsPet",
-    setup() {
-      let Icons = [
-        { link: "#", path: require("@/assets/images/contactus/Instagram.png").default },
-        { link: "#", path: require("@/assets/images/contactus/YouTube.png").default },
-        { link: "#", path: require("@/assets/images/contactus/Facebook.png").default },
-        { link: "#", path: require("@/assets/images/contactus/Telegram.png").default },
-        { link: "#", path: require("@/assets/images/contactus/TikTok.png").default },
-      ];
-  
-      return { Icons };
-    },
+    
   };
   </script>
